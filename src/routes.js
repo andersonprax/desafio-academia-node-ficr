3ea994b89c554cef9195b188f6bb39e31702fa86
andersonprax <andersonintendope@gmail.com>
2020-01-31 03:27:47 -0300

    Extraindo dados do GitHub

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,40 @@ const routes = Router()
 
 routes.get('/', (req, res, next) => res.send('Hello World Nodejs'))
 
-routes.get('/github/user', async (req, res, next) => {
-    // requisição da api publica do github utilizando axios
+routes.get('/github', async (req, res, next) => {
+    
+    // extraindo dados do github
+    let repos = []
+    let name, bio, company
+    
     try {
         const { data } = await axios.get('https://api.github.com/users/andersonprax');
-        return res.send(data);
+
+        company = data.company
+        name = data.name
+        bio = data.bio
+
     } catch (error) {
         console.log('error', error)
         return res.send(error);
     }
-})
-
-routes.get('/github/repos', async (req, res, next) => {
-    // requisição da api publica do github utilizando axios
-    try {
+        try {
         const { data } = await axios.get('https://api.github.com/users/andersonprax/repos');
-        return res.send(data);
+
+        repos = data.map(repo => {
+            let r = {
+                size: repo.size,
+                name: repo.name,
+                url: repo.url
+            }
+            return r
+        }).sort((a, b) => {
+            if (a.size < b.size)
+                return 1
+            if (a.size > b.size)
+                return -1
+            return 0;
+        }).slice(0, 3)
     } catch (error) {
         console.log('error', error)
         return res.send(error);
